fb1a60f3f16499ab5993fa1cdabd1b9e1173504a
Sean Feldman <feldman.sean@gmail.com>
2016-06-10 00:14:29 -0600

    Can check for latest version of a package

---
+++ src/Nuget.Updater.Tests/When_checking_for_latest_version.cs
@@ -0,0 +1,17 @@
+﻿namespace Nuget.Updater.Tests
+{
+    using System.Threading.Tasks;
+    using NuGet;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_checking_for_latest_version
+    {
+        [Test]
+        public async Task Should_get_it()
+        {
+            var latestVersion = await new Updater().GetLatestVersion("Phoenix");
+            Assert.That(latestVersion, Is.EqualTo(new SemanticVersion("0.0.7")));
+        }
+    }
+}
---
+++ src/Nuget.Updater/Updater.cs
@@ -0,0 +1,81 @@
+﻿namespace Nuget.Updater
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using NuGet;
+    using HttpClient = System.Net.Http.HttpClient;
+
+    public class Updater
+    {
+        Uri nugetV3Uri = new Uri("https://www.myget.org/F/messagehandler-dist/api/v3/index.json");
+
+        public async Task<SemanticVersion> GetLatestVersion(string packageId, bool includePreRelease = false, CancellationToken token = default(CancellationToken))
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var feed = await Get<Feed>(nugetV3Uri, httpClient, token);
+                var searchQueryService = feed.Resources.FirstOrDefault(x => x.Type == "SearchQueryService");
+
+                var searchPackageUri = new Uri($"{searchQueryService.Url}/?q=packageid:{packageId}&prerelease={includePreRelease}");
+                var searchResult = await Get<SearchResult>(searchPackageUri, httpClient, token);
+                var searchResultPackage = searchResult.Data.FirstOrDefault();
+                return new SemanticVersion(searchResultPackage.Version);
+            }
+        }
+
+        async Task<T> Get<T>(Uri uri, HttpClient httpClient, CancellationToken token)
+        {
+            var response = await httpClient.GetAsync(uri, token).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            var stream = await response.Content.ReadAsStreamAsync();
+            using (var streamReader = new StreamReader(stream))
+            {
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<T>(jsonTextReader);
+                }
+            }
+        }
+    }
+
+    public class Feed
+    {
+        public string Version { get; set; }
+        [JsonProperty("resources")]
+        public List<Resource> Resources { get; set; }
+    }
+
+    public class Resource
+    {
+        [JsonProperty("@id")]
+        public string Url { get; set; }
+
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+
+        public string Comment { get; set; }
+    }
+
+    public class SearchResult
+    {
+        public string Index { get; set; }
+        public DateTime LastReopen { get; set; }
+        [JsonProperty("data")]
+        public List<SearchResultPackage> Data { get; set; }
+    }
+
+    public class SearchResultPackage
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Version { get; set; }
+    }
+}
